c9c9b10d5cff32d6516fddcff2d0a8f30d2834d5
daizy <91655154+daizyjohnson@users.noreply.github.com>
2022-01-11 10:20:50 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,17 @@
+from math import sqrt
+
+if __name__="__main__":
+
+    def prime(d):
+        count = 0
+        if d in [0,1]:
+            return False
+        for i in range(2, int(sqrt(n)) +1);
+        if n % i == 0:
+            count +=1
+            if count > 0:
+                return False
+            return True
+
+a = int(input())
+print(prime(d))
